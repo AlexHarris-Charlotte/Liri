555dc11c056575029882e878ff6f4ea6501cb594
Alex Harris <akharri8@ncsu.edu>
2018-02-18 12:04:57 -0500

    Added calls to the Twitter API, Spotify API, and OMDB API

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
-require("dotenv").config();
-require("./keys"); //?
+const env = require("dotenv").config();
+const keys = require("./keys"); 
 const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
 const request = require("request");
 const fs = require("fs");
 
@@ -21,7 +22,29 @@ const nameInput = process.argv[3];
 
 
 
-const myRevealingMovieModule = (function() {
+function tweets() {
+    client.get("statuses/home_timeline", function(error, tweets, response) {
+        if(error) throw error;
+        for(var i = 0; i < tweets.length; i++) {
+            console.log(tweets[i].user.name + ": ", tweets[i].text);
+        }
+    })
+}
+
+function spotifyCall(nameInput){
+    console.log(process.argv);
+    spotify.search({ type: 'track', query: nameInput, limit: '1' }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+      console.log("Song Name: " + data.tracks.items[0].name);
+      console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name);
+      console.log("Album Name: " + data.tracks.items[0].album.name);
+      console.log("URL link to the album on Spotify: " + data.tracks.items[0].external_urls.spotify);
+      });
+}
+
+function movieFinder(nameInput) {
     let queryUrl = "https://www.omdbapi.com/?t=" + nameInput + "&y=&plot=short&apikey=trilogy";
     request(queryUrl, function(error, response, body) {
         if (!error && response.statusCode === 200) {
@@ -35,22 +58,15 @@ const myRevealingMovieModule = (function() {
             console.log("Some of the Actors involved: " + JSON.parse(body).Actors);
         }
     })
-})(nameInput);
-
-
-function tweets() {
-    let queryUrl = "https://api.twitter.com/1.1/search/tweets.json?q=akharri8"
-}
-
-
+};
 
 // Using == because we don't have to worry about data type. process.argv only returns strings
 if(operation == "my-tweets") {
-    // Some Function here
+    tweets();
 } else if(operation == "spotify-this-song" && nameInput != undefined) {
-    // Some Function here
+    spotifyCall(nameInput);
 } else if(operation == "movie-this" && nameInput != undefined) {
-    myRevealingMovieModule; 
+    movieFinder(nameInput); 
 } else if(operation == "do-what-it-says") {
     // Some Function here
 } else {
